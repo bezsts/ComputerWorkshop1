94b213bc2725982282a47842b4c66e87fa4a6c23
bezst <bezst29@gmail.com>
2024-10-19 15:41:15 +0300

    completed storages

--- ComputerWorkshop1/Storage/MovieStorage.cs
+++ ComputerWorkshop1/Storage/MovieStorage.cs
@@ -1,6 +1,48 @@
-﻿namespace ComputerWorkshop1.Storage
+﻿using ComputerWorkshop1.Models;
+
+namespace ComputerWorkshop1.Storage
 {
     public class MovieStorage
     {
+        private readonly JsonFileService<Movie> jsonFileService = new JsonFileService<Movie>("Data/movies.json");
+        private List<Movie> data = [];
+
+        public MovieStorage() 
+        { 
+            data = jsonFileService.LoadData();
+        }
+
+        public IEnumerable<Movie> GetAll() => data;
+
+        public Movie? Get(int id) => data.Find(x => x.Id == id);
+
+        public Movie Add(Movie movie)
+        {
+            movie.Id = data.Any() ? data.Max(x => x.Id) + 1 : 1;
+            data.Add(movie);
+
+            return movie;
+        }
+
+        public bool Update(int id, Movie updatedMovie)
+        { 
+            var movieIndex = data.FindIndex(x => x.Id == id);
+
+            if (movieIndex == -1)
+            {
+                return false;
+            }
+
+            data[movieIndex].Title = updatedMovie.Title;
+            data[movieIndex].Director = updatedMovie.Director;
+            data[movieIndex].Genre = updatedMovie.Genre;
+            data[movieIndex].ReleaseDate = updatedMovie.ReleaseDate;
+
+            return true;
+        }
+
+        public void Delete(int id) => data.RemoveAll(x => x.Id == id);
+
+        public void SaveData() => jsonFileService.SaveData(data);
     }
 }
--- ComputerWorkshop1/Storage/UserStorage.cs
+++ ComputerWorkshop1/Storage/UserStorage.cs
@@ -4,6 +4,44 @@ namespace ComputerWorkshop1.Storage
 {
     public class UserStorage
     {
-        
+        private readonly JsonFileService<User> jsonFileService = new JsonFileService<User>("Data/users.json");
+        private readonly List<User> data = [];
+
+        public UserStorage() 
+        {
+            data = jsonFileService.LoadData();
+        }
+
+        public IEnumerable<User> GetAll() => data;
+
+        public User? Get(int Id) => data.Find(x => x.Id == Id);
+
+        public User Add(User user)
+        {
+            user.Id = data.Any() ? data.Max(x => x.Id) + 1 : 1;
+            data.Add(user);
+
+            return user;
+        }
+
+        public bool Update(int id, User updatedUser)
+        { 
+            var userIndex = data.FindIndex(x => x.Id == id);
+
+            if (userIndex == -1)
+            {
+                return false;
+            }
+
+            data[userIndex].Name = updatedUser.Name;
+            data[userIndex].Email = updatedUser.Email;
+            data[userIndex].Password = updatedUser.Password;
+            data[userIndex].WatchedMovies = updatedUser.WatchedMovies;
+
+            return true;
+        }
+        public void Delete(int Id) => data.RemoveAll(x => x.Id == Id);
+
+        public void SaveData() => jsonFileService.SaveData(data);
     }
 }
