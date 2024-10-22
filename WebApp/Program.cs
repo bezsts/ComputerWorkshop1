334dacccfd83bff05c33326f027e2b8c2dd6818f
bezst <bezst29@gmail.com>
2024-10-22 22:09:48 +0300

    test

---
+++ WebApp/Program.cs
@@ -0,0 +1,42 @@
+using ApiDomain.Services;
+using Microsoft.OpenApi.Models;
+using System.Reflection;
+
+namespace ComputerWorkshop1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddControllers();
+            builder.Services.AddUser();
+            builder.Services.AddMovie();
+            builder.Services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "Catalog of watched films API",
+                    Description = "An ASP.NET Core Web API for managing a catalog of watched films"
+                });
+
+                options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory,
+                    $"{Assembly.GetExecutingAssembly().GetName().Name}.xml"));
+            });
+
+            var app = builder.Build();
+
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
